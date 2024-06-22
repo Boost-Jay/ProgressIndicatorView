ecee199614537e56f482c60f4faf2219c011e748
MiTJJ_Bs <104751976+Boost-Jay@users.noreply.github.com>
2024-06-22 10:11:02 +0800

    Update BarView.swift

--- Sources/Indicators/BarView.swift
+++ Sources/Indicators/BarView.swift
@@ -6,19 +6,18 @@
 //  Copyright © 2021 Exyte. All rights reserved.
 //
 
-import SwiftUI
-
 struct BarView: View {
-    
     @Binding var progress: CGFloat
     let backgroundColor: Color
-    
+    let foregroundColor: Color = .red  // 這裡設置預設的前景色為紅色
+
     var body: some View {
         GeometryReader { geometry in
             ZStack(alignment: .leading) {
                 Capsule()
                     .foregroundColor(backgroundColor)
                 Capsule()
+                    .foregroundColor(foregroundColor)  // 使用指定的前景色
                     .frame(width: min(max(geometry.size.width * progress, 0), geometry.size.width))
                     .animation(.easeIn, value: progress)
             }
